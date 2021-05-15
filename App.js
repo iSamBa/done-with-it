bbc8dd111beac4d01591e71937d7154f5c5b29fe
iSamBa <bahou.aissam@gmail.com>
2021-05-15 18:58:29 +0200

    implement the ViewImageScreen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {  StyleSheet } from "react-native";
+import ViewImageScreen from './app/screens/ViewImageScreen.js';
 
 import WelcomeScreen from './app/screens/WelcomeScreen.js'
 export default function App() {
   return (
-    <WelcomeScreen />
+    <ViewImageScreen />
   );
 }
 
--- app/config/colors.js
+++ app/config/colors.js
@@ -1,4 +1,6 @@
 export default {
   primary: "#fc5c65",
-  secondary : '#4ECDC4'
+  secondary : '#4ECDC4',
+  dark : '#000',
+  light : '#fff'
 }
