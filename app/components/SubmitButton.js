b21be80e94e5dae2564074b529835ac976aa88ba
iSamBa <bahou.aissam@gmail.com>
2021-05-21 15:42:47 +0200

    fix import for SubmitButton

--- app/components/SubmitButton.js
+++ app/components/SubmitButton.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useFormikContext } from 'formik'
 
+import AppButton from './AppButton'
+
 function SubmitButton({ title }) {
     const {handleSubmit} = useFormikContext()
     return (
