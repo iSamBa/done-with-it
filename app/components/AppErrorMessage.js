ba70a1b4c78a77eaee7c7e27e4aef53a1a8dd557
iSamBa <bahou.aissam@gmail.com>
2021-05-21 15:22:08 +0200

    add touched state management loginScreen componenets

--- app/components/AppErrorMessage.js
+++ app/components/AppErrorMessage.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { StyleSheet } from 'react-native'
 import AppText from './AppText'
 
-function AppErrorMessage({ error }) {
-    if (!error) return null
+function AppErrorMessage({ error , visible}) {
+    if (!visible  || !error) return null
     return (
         <AppText style={styles.error}>{ error }</AppText>
     )
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -26,28 +26,30 @@ function LoginScreen() {
                 onSubmit={values => console.log(values)}
                 validationSchema={validationSchema}
             >
-                {({ handleChange, handleSubmit, errors }) => 
+                {({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => 
                     <>
                         <AppTextInput
                             autoCapitalize='none'
                             autoCorrect={false}
                             icon="email"
                             keyboardType="email-address"
+                            onBlur={()=>setFieldTouched('email')}
                             onChangeText= {handleChange('email')}
                             placeholder="Email"
                             textContentType="emailAddress"
-                        />
-                        <AppErrorMessage error={errors.email} />
+                            />
+                        <AppErrorMessage error={errors.email} visible={touched.email}/>
                         <AppTextInput
                             autoCapitalize='none'
                             autoCorrect={false}
                             icon="lock"
+                            onBlur={()=>setFieldTouched('password')}
                             onChangeText= {handleChange('password')}
                             placeholder="Password"
                             secureTextEntry
                             textContentType="password"
                         />
-                        <AppErrorMessage error={errors.password} />
+                        <AppErrorMessage error={errors.password} visible={touched.password} />
                         <AppButton title="login" onPress={handleSubmit}/>
                     </>
                 }
