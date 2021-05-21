bf0edc5a6854598b42d12b258f526a9b7b4b238e
iSamBa <bahou.aissam@gmail.com>
2021-05-21 19:36:13 +0200

    create register screen

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {  StyleSheet  } from "react-native";
 
-import LoginScreen from './app/screens/LoginScreen';
-
-
+import RegisterScreen from './app/screens/RegisterScreen';
 
 export default function App() {
   return (
-      <LoginScreen></LoginScreen> 
+      <RegisterScreen></RegisterScreen> 
     );
 }
 
