ab31164b3e5ff47487927ddb1bef4d3ac48805f6
iSamBa <bahou.aissam@gmail.com>
2021-05-28 23:59:25 +0200

    refactor code in AuthNavigator component

--- App.js
+++ App.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import AuthNavigator from './app/navigation/AuthNavigator';
 
 
-import LoginScreen from './app/screens/LoginScreen';
-import RegisterScreen from './app/screens/RegisterScreen';
-import Screen from './app/components/Screen'
-import WelcomeScreen from './app/screens/WelcomeScreen'
 
 
-const Stack = createStackNavigator();
 
 export default function App() {
   
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-      </Stack.Navigator>
+     <AuthNavigator />
     </NavigationContainer>
     );
 }
